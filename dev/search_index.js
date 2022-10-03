4264a4e94ad6f70f606e7c83ae53d9750d376a51
Documenter.jl <documenter@juliadocs.github.io>
2022-10-03 14:04:02 +0000

    build based on d754c63

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FlatGeometry","category":"page"},{"location":"#FlatGeometry","page":"Home","title":"FlatGeometry","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FlatGeometry.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FlatGeometry]","category":"page"}]
+}
